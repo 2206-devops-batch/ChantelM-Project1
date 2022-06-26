7772c361cb2700a7e0c0d9c4cd1cbd8f8eb950c2
cm <cm@pop-os.localdomain>
2022-06-25 22:05:57 -0700

    successful local deployment using docker network

--- src/client/app.py
+++ src/client/app.py
@@ -1,12 +1,32 @@
 # python3 -m flask run
 # ctrl+c
 
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask, render_template
+import datetime
+import psycopg2
 
-@app.route("/")
-def home():
-    return "Hello from Docker & Flask!"
+app = Flask(__name__, template_folder='./templates')
+
+def get_db_connection():
+    conn = psycopg2.connect(host='testnet',
+                            port='5432',
+                            database='test',
+                            user='postgres',
+                            password='password')
+    return conn
+
+
+@app.route('/')
+def hello():
+    conn = get_db_connection()
+    cur = conn.cursor()
+    cur.execute('SELECT * FROM camps;')
+    camps = cur.fetchall()
+    cur.close()
+    conn.close()
+    print(camps)
+    return render_template('index.html', camps=camps)
 
 if __name__ == '__main__':
+    # helps expose container to localhost
     app.run(debug=True, host='0.0.0.0')
