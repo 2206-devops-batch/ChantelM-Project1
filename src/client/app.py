c8d3ab34ae4bf8845dfea38a906583b9ae94cd5c
cm <cm@pop-os.localdomain>
2022-06-26 09:26:05 -0700

    initial incorporation of .dev environment

--- src/client/app.py
+++ src/client/app.py
@@ -22,15 +22,20 @@ def get_db_connection():
 
 @app.route('/')
 def hello():
-    camps=['camp1', 'camp2']
-    if curenv=='DEVELOPMENT':
-        conn = get_db_connection()
-        cur = conn.cursor()
-        cur.execute('SELECT * FROM camps;')
-        camps = cur.fetchall()
-        cur.close()
-        conn.close()
+    camps=['camp0']
+
+    if curenv == 'DEVELOPMENT':
+        try:
+            conn = get_db_connection()
+            cur = conn.cursor()
+            cur.execute('SELECT * FROM camps;')
+            camps = cur.fetchall()
+            cur.close()
+            conn.close()
+        except:
+            camps = ['camp3', 'camp4']
+
     return render_template('index.html', camps=camps)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0')
