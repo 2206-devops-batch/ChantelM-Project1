293816db18e3d3aee7ebddad3748b01150660a8a
cm <cm@pop-os.localdomain>
2022-06-26 00:31:32 -0700

    docker-compose-dev.yml issues with network aliases. flask setup for development .env

--- src/client/app.py
+++ src/client/app.py
@@ -2,31 +2,35 @@
 # ctrl+c
 
 from flask import Flask, render_template
-import datetime
+from dotenv import dotenv_values
 import psycopg2
 
+config = dotenv_values(".env")
+curenv = config['CURENV']
 app = Flask(__name__, template_folder='./templates')
 
 def get_db_connection():
-    conn = psycopg2.connect(host='testnet',
-                            port='5432',
-                            database='test',
-                            user='postgres',
-                            password='password')
+    conn = None
+    if curenv=='DEVELOPMENT':
+        conn = psycopg2.connect(host=config['DB_DEV_HOST'],
+                                port=config['DB_DEV_PORT'],
+                                database=config['DB'],
+                                user=config['DB_USER'],
+                                password=config['DB_PASSWORD'])
     return conn
 
 
 @app.route('/')
 def hello():
-    conn = get_db_connection()
-    cur = conn.cursor()
-    cur.execute('SELECT * FROM camps;')
-    camps = cur.fetchall()
-    cur.close()
-    conn.close()
-    print(camps)
+    camps=['camp1', 'camp2']
+    if curenv=='DEVELOPMENT':
+        conn = get_db_connection()
+        cur = conn.cursor()
+        cur.execute('SELECT * FROM camps;')
+        camps = cur.fetchall()
+        cur.close()
+        conn.close()
     return render_template('index.html', camps=camps)
 
 if __name__ == '__main__':
-    # helps expose container to localhost
-    app.run(debug=True, host='0.0.0.0')
+    app.run(debug=True)
