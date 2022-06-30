c0fb5b89dfcdb1759e2755667b6d5a7effdb87a6
chamoo334 <moore.cmarie@gmail.com>
2022-06-29 22:14:15 -0700

    ansible files

--- README.md
+++ README.md
@@ -4,4 +4,5 @@ Web application to manage future camping locations
 [Flask](https://flask.palletsprojects.com/en/2.1.x/)
 [Flask-Bootstrap](https://pythonhosted.org/Flask-Bootstrap/)
 [Docker](https://www.docker.com/)
-Flask-WTF
+[Flask-WTF](https://flask-wtf.readthedocs.io/en/1.0.x/)
+[Psycopg2](https://www.psycopg.org/docs/)
--- src/client/tests/test_example.py
+++ src/client/tests/test_example.py
@@ -1,16 +1,12 @@
-import sys
-sys.path.append("../")
-
-import app
-import pytest
-
 # https://docs.pytest.org/en/7.1.x/
 # flask testing with pytest: https://flask.palletsprojects.com/en/2.1.x/testing/
+
+# pytest tests/
 
 def inc(x):
     return x + 1
 
 
 def test_answer():
-    assert inc(3) == 5
+    assert inc(3) == 4
 
